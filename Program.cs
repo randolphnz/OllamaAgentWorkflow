e7ec92b1d2f5225fcf2088b0fc8dbd33623fcbea
Randy Wu <Randy.Wu@callaghaninnovation.govt.nz>
2026-01-09 21:56:41 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Workflows;
+using OllamaSharp;
+
+try
+{
+    Uri ollamaUri = new Uri("http://localhost:11434");
+    string modelName = "llama2:7b";
+    OllamaApiClient ollamaClient = new OllamaApiClient(ollamaUri, modelName);
+
+    ChatClientAgent writer = new ChatClientAgent(ollamaClient, name: "Writer", instructions: "you write engaging stories based on a topic. Be creative, positive, and keep it under 200 words.");
+    ChatClientAgent editor = new ChatClientAgent(ollamaClient, name: "Editor", instructions: "you review a story for clarity, grammar, and style, then refine it.");
+    Workflow workflow = AgentWorkflowBuilder.BuildSequential(writer, editor);
+    AIAgent workflowAgent = workflow.AsAgent();
+
+    string topic = "A day in Auckland New Zealand.";
+    Console.WriteLine($"--- Starting workflow for Topic {topic} ---\n");
+    AgentRunResponse response = await workflowAgent.RunAsync(topic);
+    Console.WriteLine("--- Final Output ---");
+    Console.WriteLine(response.Text);
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
