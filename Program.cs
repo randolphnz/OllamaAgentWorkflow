f0000667272c345e7156121ac50cb82254b25f78
Randy Wu <Randy.Wu@callaghaninnovation.govt.nz>
2026-01-24 22:30:46 +1300

    Add MCP

--- Program.cs
+++ Program.cs
@@ -1,30 +1,42 @@
 ﻿using Microsoft.Agents.AI;
-using Microsoft.Agents.AI.Workflows;
-using OllamaSharp;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+using OllamaSharp;
+using OllamaSharp.Models.Chat;
 
 IConfigurationRoot configBuilder = new ConfigurationBuilder().AddJsonFile("appconfig.json").Build();
 IConfigurationSection configSection = configBuilder.GetSection("AppSettings");
 string uri = configSection["OllamaURI"] ?? throw new Exception("OllamaURI not found in configuration.");
 string modelName = configSection["ModelName"] ?? throw new Exception("ModelName not found in configuration.");
-string writerInstructions = configSection["WriterAgentInstructions"] ?? throw new Exception("WriterInstructions not found in configuration.");
-string editorInstructions = configSection["EditorAgentInstructions"] ?? throw new Exception("EditorInstructions not found in configuration.");
+string dateExtractorInstructions = configSection["DateExtractorInstructions"] ?? throw new Exception("DateExtractorInstructions not found in configuration.");
+string mcpProjectPath = configSection["MCPProjectPath"] ?? throw new Exception("MCPProjectPath not found in configuration.");
+string mcpProjectName = configSection["MCPProjectName"] ?? throw new Exception("MCPProjectName not found in configuration.");
 Uri ollamaUri = new Uri(uri);
 
 try
 {   
-    OllamaApiClient ollamaClient = new OllamaApiClient(ollamaUri, modelName);    
-    ChatClientAgent writer = new ChatClientAgent(ollamaClient, name: "Writer", instructions: writerInstructions);   
-    ChatClientAgent editor = new ChatClientAgent(ollamaClient, name: "Editor", instructions: editorInstructions);
-    Workflow workflow = AgentWorkflowBuilder.BuildSequential(writer, editor);
-    AIAgent workflowAgent = workflow.AsAgent();
+    OllamaApiClient ollamaClient = new OllamaApiClient(ollamaUri, modelName);
+    ChatClientAgent dateExtractorAgent = new ChatClientAgent(ollamaClient, name: "DateExtractor", instructions: dateExtractorInstructions);    
+    Console.Write("Prompt: ");
+    string chatMessage = Console.ReadLine() ?? throw new Exception("Prompt is empty.");
+    AgentRunResponse response = await dateExtractorAgent.RunAsync(chatMessage);
+    Console.WriteLine($"The date to be booked: {response.Text.Trim()}");
+    string extractedDate = response.Text.Trim();
 
-    string topic = "A day in Hangzhou Zhejiang province China.";
-    Console.WriteLine($"--- Starting to write a story for topic '{topic}' ---\n");
-    AgentRunResponse response = await workflowAgent.RunAsync(topic);
-    Console.WriteLine("--- Final Output ---");
-    Console.WriteLine(response.Text);
+    //Connect to MCP server
+    StdioClientTransport clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+    {
+        Name = mcpProjectName,
+        Command = "dotnet run",
+        Arguments = ["--project", mcpProjectPath],
+    });
+
+    McpClient mcpClient = await McpClient.CreateAsync(clientTransport);
+    Console.WriteLine($"Connected to server: {mcpClient.ServerInfo.Name}");
+    CallToolResult result = await mcpClient.CallToolAsync("book_calendar", new Dictionary<string, object?>() {["message"] = extractedDate}, cancellationToken: CancellationToken.None);
+    Console.WriteLine($"{result.Content.OfType<TextContentBlock>().First().Text})");
 }
 catch(Exception ex)
 {
